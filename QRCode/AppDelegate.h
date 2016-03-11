61a9569ef4ea16680ccb73bd95be0597a7778abe
JustToBeBetter <li@li.local>
2016-03-11 15:23:17 +0800

    Initial Commit

---
+++ QRCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRCode
+//
+//  Created by 李金柱 on 16/3/11.
+//  Copyright © 2016年 likeme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCode
+//
+//  Created by 李金柱 on 16/3/11.
+//  Copyright © 2016年 likeme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
